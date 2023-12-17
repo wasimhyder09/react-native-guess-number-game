4e82b39a2053d979382de672d52e9016bcbc2c66
Wasim Hyder <wasim@creativefaze.com>
2023-12-17 20:08:30 +0500

    Added background gradient via expo-linear-gradient package

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import StartGameScreen from './screens/StartGameScreen';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
   return (
-    <StartGameScreen />
+    <LinearGradient style={styles.rootScreen} colors={['#4e0329', '#ddb52f']}>
+      <StartGameScreen />
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
+  rootScreen: {
+    flex: 1
+  }
 });
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -32,7 +32,7 @@ const styles = StyleSheet.create({
     padding: 16,
     marginHorizontal: 24,
     marginTop: 100,
-    backgroundColor: '#4e0329',
+    backgroundColor: '#3b021f',
     borderRadius: 8,
     elevation: 4,
     shadowColor: 'black',
